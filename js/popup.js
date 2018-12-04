914aa1b110ddcd5f73fc5b88d7d0d781b3e8f21f
krdlab <krdlab@gmail.com>
2018-12-04 22:36:51 +0900

    ユーザーの設定を保存する

--- js/popup.js
+++ js/popup.js
@@ -3,11 +3,52 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-$(document).ready(function() {
+const storeShortcuts = (shortcuts, callback) => {
+  chrome.storage.local.set({shortcuts}, callback);
+};
+
+const restoreConfig = callback => {
+  chrome.storage.local.get(['shortcuts'], result => {
+    if (result && $.isArray(result.shortcuts)) {
+      callback(result.shortcuts);
+    }
+  });
+};
+
+const setShortcuts = shortcuts => {
   chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, {method: 'get_domain_list'}, (res) => {
-      res.forEach(d => $('select').append(`<option value="${d.id}">${d.name}</option>`));
-      $('select').formSelect();
+    chrome.tabs.sendMessage(tabs[0].id, {method: 'set_shortcuts', args: shortcuts}, () => {});
+  });
+};
+
+const getDomainList = callback => {
+  chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, {method: 'get_domain_list'}, list => {
+      if (list && $.isArray(list)) {
+        callback(list);
+      }
+    });
+  });
+};
+
+const resolveTextInputFromSelect = $it => $(`#${$it.attr('name')}_display_name`);
+
+$(document).ready(function() {
+  getDomainList(list => {
+    const $select = $('select');
+    list.forEach(d => $select.append(`<option value="${d.id}">${d.name}</option>`));
+    $select.formSelect();
+
+    restoreConfig(conf => {
+      conf.forEach((sc, i) => {
+        if (i < $select.length) {
+          const $it = $select.eq(i);
+          $it.val(sc.domainId);
+          resolveTextInputFromSelect($it).val(sc.displayName);
+        }
+      });
+      $select.formSelect();
+      setShortcuts(conf);
     });
   });
 
@@ -15,12 +56,13 @@ $(document).ready(function() {
     const shortcuts = [];
     $('select').each(function() {
       const $select = $(this);
-      const name = $select.attr('name');
-      const $input = $(`#${name}_display_name`);
+      const $input  = resolveTextInputFromSelect($select);
       shortcuts.push({domainId: $select.val(), displayName: $input.val()});
     });
-    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-      chrome.tabs.sendMessage(tabs[0].id, {method: 'set_shortcuts', args: shortcuts}, () => {});
-    });
+    setShortcuts(shortcuts);
+
+    const $status = $('#shortcuts_status');
+    $status.text('saving...');
+    storeShortcuts(shortcuts, () => $status.text('saved!'));
   });
 });
